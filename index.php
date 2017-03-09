6d7bab14702ca41484b27ff57d8dc4996b9effae
ahmedelzayat <ahmeffawzy9@gmail.com>
2017-03-09 15:23:16 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,46 +7,35 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>gmail</title>
+        <title></title>
     </head>
-    <body><center>
-        <form method="post"action="">
-        <label>userame</label>
-        <input type='username'name='username'><br>
-         <label>password</label>
-        <input type='pass'name='password'><br>
-         <label>repeat password</label>
-        <input type='pass'name='repeat password'><br>
-        
-         <label>email</label>
-         <input type='text'name='email'><br>
-         <label>sex</label>
-         <select>
-              <option>male</option>
-               <option>female</option>
-         </select><br>
-         <input type='submit'name='ok'value='send'>
-         </center>
+    <body>
+        <form actuon=""enctype="" method="post">
+            <fieldset>
+                <legend>login information</legend>
+            <input type="text"name="username"placeholder="enter username"maxlength="15"/>
+            <input type="password"name="password"placeholder="enter password"minlength="10"maxlength="20"/><br>
+            </fieldset>
+            <textarea name="information"placeholder="enter information"></textarea><br>
+            <select name="select">
+             <option value="2006"/>2006
+             <option value="2005"/>2005
+             <option value="2004"/>2004
+             <option value="2003"/>2003
+            </select>
+            <br>
+            <input type="radio" name="rd1" value="1"/> radio button
+            <input type="radio" name="rd1" value="81"/> radio button
+            <br>
+            <input type="checkbox" name="ch" value="1"/> checkbox1
+            <input type="checkbox" name="ch" value="0"/> checkbox1
+            <br>
+            <input type="file"name="fille"/>
+            <br>
+            <input type="submit"name="submit"value="submit"/>
+            
         </form>
-         <?php
-         mysql_connect("127..0.0.1","root","");
-         mysql_select_db("ahmed");
-         if (isset($_POST[ok])){
-         $username=$_POST['username'];
-         $password=$_POST['password'];
-         $rapeatpassword=$_POST['rapeatpassword'];
-         $sex=$_POST['sex'];
-         $email=$_POST['email'];
-         mysql_quary("insert into ahmed value('','$username','$password',' $rapeatpassword','$sex','$email')");
-         
-                 
-         }
-             
-     
-         
-                 
-         
-         
+        <?php
         // put your code here
         ?>
     </body>
